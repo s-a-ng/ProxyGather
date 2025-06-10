e137bd6a24038d1528aced21c9963f1b47ff6eb7
Skillter <57605954+Skillter@users.noreply.github.com>
2025-06-10 08:02:36 +0200

    Possible new experimental sources from a different project

--- getproxy/getproxy-install.bat
+++ getproxy/getproxy-install.bat
@@ -9,5 +9,8 @@ go install -v github.com/mubeng/mubeng@latest
 echo Downloading ProxyScraper.py
 curl -o scrapers/ProxyScraper-original.py https://raw.githubusercontent.com/SIDDHU123M/Ultimate-Proxy-Scraper/refs/heads/main/ProxyScraper.py
 
+git clone https://github.com/chill117/proxy-lists.git
+cd proxy-lists
+npm install
 
 timeout 5
